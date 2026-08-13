e77250868366eb943de2e59be6a4cd952f6818e2
Aniket Raj <araj52444@gmail.com>
2026-08-13 15:30:39 +0530

    Add initial implementation of hello.py and variable.py with basic comments and data type examples

---
+++ hello.py
@@ -0,0 +1,30 @@
+# This is a comment
+'''
+This is a comment
+written in
+more than just one line
+'''
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+
+# print("hello")
+
+# print(print.__doc__)
+
+
+def sum(a,b):
+    """This function will accept two numbvers and add those numbers"""
+    print(a+b)
+
+sum(10,30)
+
+# print(help(sum))
+
+
+print("Hello", end="-")
+print("World")
+
---
+++ variable.py
@@ -0,0 +1,54 @@
+name = "Aniket"
+age = 20
+pi = 3.14
+is_student = False
+
+a_b_c = "abc"
+
+type_of_pi = type(pi)
+print(type_of_pi)
+
+
+"""
+Data Types
+String - str
+Integer - int
+Float - float
+Complex (Real + Imaginary) - complex
+Boolean - bool
+None - None
+"""
+
+#String
+#double quote
+city = "Noida"
+#single quote
+country = 'India'
+#triple (double/single) quote
+college = """GLA University"""
+year = '''2026'''
+
+#Numbers
+#int
+a = 10
+#float
+b = 5.4
+#complex
+c = 3 + 4j
+
+#Boolean
+x = True #1
+y = False #0
+
+#None
+z = None
+
+print(isinstance(x,bool)) #Data, DataType -> Boolean(True/False)
+
+number = "10"
+new_number = int(number)
+c_number = complex(new_number)
+print(c_number)
+print(isinstance(True, int))
+print(bool([1,2,3]))
+
