9385841c1575198f9bba0e0834aa7e7e7cbfe1ee
Aniket Raj <araj52444@gmail.com>
2026-08-18 10:44:23 +0530

    Add input handling for number inputs and string concatenation in input.py

---
+++ input.py
@@ -0,0 +1,13 @@
+# name = input()
+# print("After input")
+# print("Entered Name is",name)
+
+# age = input("Enter your age ")
+# new_age = int(age)
+# print(new_age == 15)
+
+
+num1 = input("Enter first number ")
+num2 = input("Enter second number ")
+sum = num1 + num2 #string concatination because num1 and num2 are string
+print(sum)
