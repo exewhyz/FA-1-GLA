e7feabc6b58b2da1a6a1dfee4490121841cd9561
Aniket Raj <araj52444@gmail.com>
2026-08-19 15:13:01 +0530

    examples of input

--- input.py
+++ input.py
@@ -7,7 +7,29 @@
 # print(new_age == 15)
 
 
-num1 = input("Enter first number ")
-num2 = input("Enter second number ")
-sum = num1 + num2 #string concatination because num1 and num2 are string
-print(sum)
+# num1 = input("Enter first number ")
+# num2 = input("Enter second number ")
+# sum = num1 + num2 #string concatination because num1 and num2 are string
+# print(sum)
+
+
+# base = float(input("Enter value of base: "))
+# height = float(input("Enter value of height: "))
+# area = 1/2*base * height
+# print(area)
+
+# a,b,c = input("enter 3 values separated by space: ").split() #5-6-7 -> "5 6 7" -> ["5","6","7"]
+# print("A", a)
+# print("B", b)
+# print("C", c)
+# name = "Aniket"
+# print("hello", name, "How are you")
+# print("hello name How are you")
+# print(f"Hello {name} How are you")
+# print("Area of triangle is {area:.2f}")
+
+
+# print(1, end="@")
+# print(2)
+
+# print("hello","aniket",sep=None)
