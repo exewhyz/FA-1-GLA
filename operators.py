547a9e27db495b735163d6bd6c9066b9709443e3
Aniket Raj <araj52444@gmail.com>
2026-08-22 14:02:24 +0530

    Add initial implementation of operators.py with examples of arithmetic, assignment, comparison, and logical operators

---
+++ operators.py
@@ -0,0 +1,66 @@
+#opeartion
+#operand
+#operator
+
+# 3 * 4
+
+"""
+Operator Types
+
+1. Arithmetic Operator
+2. Assignment Operator
+3. Comparision Operator
+4. Logical Operator
+"""
+
+# Arithmetic Operator
+n1 = 3
+n2 = 2
+
+add = n1 + n2
+sub = n1 - n2
+mul = n1 * n2
+div = n1 / n2
+remainder = n1 % n2
+power = n1 ** n2
+floor_div = n1 // n2
+
+# print(f"Div: {div}, Type: {type(div)}")
+
+# print(f"Remainder: {remainder}, Type: {type(remainder)}")
+
+# print(f"Power: {power}, Type: {type(power)}")
+
+# print(f"Floor Div: {floor_div}, Type: {type(floor_div)}")
+
+
+# Assinment Operator
+
+n = 4
+m = 5
+n -= m
+n *= 2.5
+n //= 1
+# print(n)
+
+# Comparision Operator (==, !=, >, <, >=, <=)
+
+c1 = 4
+c2 = 6
+
+# print(c1 == c2)
+# print(c1 != c2)
+
+# Logical Operator (not, and, or)
+
+# print(True and False)
+# print(True and False and True)
+# print(0 and False)
+# print("Aniket" and 3-3 and None)
+
+# print(False or False)
+# print(not None or 1)
+
+# print("Hello" and not 1049 or "hi" and "")
+# ("Hello" and False) or ("hi" and "")
+# False or ""
