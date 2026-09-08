1820253d838e73a5789c4a40c6ce4e52e3222af4
Aniket Raj <araj52444@gmail.com>
2026-09-08 12:05:03 +0530

    Add initial implementation of tuple.py with examples of tuple operations and unpacking

---
+++ tuple.py
@@ -0,0 +1,44 @@
+# [ 1,2,3 ]  #List
+
+#Tuple
+t = ( 1, 2, 3, "Hello", True,3, ( "devansh" ), ["one", "hii"] )
+
+# print(t[2:6])
+# t.extend(6,len(t)) #AttributeError
+# t[3] = "Bye" #TypeError
+# del t[3] #TypeError
+
+# print(t.index(True,3,7))
+
+# tuple packing
+nums = 1,2,3
+# nums = (1,2,3)
+# print(type(nums))
+
+# tuple/list unpacking
+# a,b,c = [4, 5, 6]
+
+# h=input("Enter 3 values: ").split(" ")
+
+# j,k,l = h
+
+# p[0]
+# p[1]
+# p[2]
+
+# d = 4
+# e = 5
+# f = 6
+d,e,f = 4, 5, 6
+# d,e,f = (4, 5, 6) # first packing into tuple and then unpacking of values
+
+
+g = "hello"
+
+# print(tuple(g))
+
+f = (5, 3,6)
+
+# f.sort()
+print(sorted(f))
+
